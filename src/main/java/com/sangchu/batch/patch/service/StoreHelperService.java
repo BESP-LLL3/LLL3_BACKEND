c7a8f078c8acb00a8eb9b872ad0201b0040397da
KIMB0B <axzsd96301@gmail.com>
2025-04-22 10:57:14 +0900

    Feat: 비동기 적용 후MySQL 데이터 읽는 방식을 페이지네이션에서 id기반으로 변경 1. Step에서 taskExecutor()로 비동기 적용 2. StoreRepository에서 id순서로 데이터를 가져오도록 하여 읽기 성능 개선

--- src/main/java/com/sangchu/batch/patch/service/StoreHelperService.java
+++ src/main/java/com/sangchu/batch/patch/service/StoreHelperService.java
@@ -14,8 +14,16 @@ import java.util.List;
 public class StoreHelperService {
     private final StoreRepository storeRepository;
 
-    public List<Store> getStoreWithoutFranchise(int page, int size) {
-        return storeRepository.findAllByBranchNmIsNullOrBranchNmEquals("", PageRequest.of(page, size)).getContent();
+    // 페이지네이션을 사용하여 프랜차이즈가 없는 매장 목록을 가져오는 메서드
+//    public List<Store> getStoreWithoutFranchise(int page, int size) {
+//        return storeRepository.findAllByBranchNmIsNullOrBranchNmEquals("", PageRequest.of(page, size)).getContent();
+//    }
+
+    // ID 기반으로 프랜차이즈가 없는 매장 목록을 가져오는 메서드
+    public List<Store> getStoreWithoutFranchise(Long lastId, int size) {
+        // lastId가 null이면 0으로 초기화
+        Long cursor = lastId != null ? lastId : 0;
+        return storeRepository.findStoresAfterId("", Math.toIntExact(cursor), size);
     }
 }
 
--- src/main/java/com/sangchu/batch/preprocess/config/StoreJobConfig.java
+++ src/main/java/com/sangchu/batch/preprocess/config/StoreJobConfig.java
@@ -12,7 +12,9 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -48,6 +50,18 @@ public class StoreJobConfig {
                 .faultTolerant()
                 .skip(Exception.class)
                 .skipLimit(100)
+                .taskExecutor(taskExecutor())
                 .build();
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4); // 스레드 풀 크기
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(10);
+        executor.setThreadNamePrefix("store-task-");
+        executor.initialize();
+        return executor;
     }
 }
