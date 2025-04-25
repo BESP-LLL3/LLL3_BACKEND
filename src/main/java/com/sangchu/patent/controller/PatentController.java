57ab8321851c2cc47c941f32147b89e0dc85a034
김진형 <332bro@naver.com>
2025-04-25 17:10:38 +0900

    Feat: 상호명 중복 확인 api 만들기 (#30)
    * Feat: 상호명 중복 확인 API 만들기
    
    1. 상호명 중복 확인 API 개발
    2. elasticsearch 배치 컨트롤러 최신 분기 파라미터로 받아 저장하도록 수정
    
    * Refactor: 중복 확인 컨트롤러 메서드 명 수정

---
+++ src/main/java/com/sangchu/patent/controller/PatentController.java
@@ -0,0 +1,30 @@
+package com.sangchu.patent.controller;
+
+import com.sangchu.global.mapper.ResponseMapper;
+import com.sangchu.global.response.BaseResponse;
+import com.sangchu.global.util.statuscode.ApiStatus;
+import com.sangchu.patent.service.PatentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class PatentController {
+    private final PatentService patentService;
+
+    @GetMapping("/patent")
+    public ResponseEntity<BaseResponse<Boolean>> checkPatent(@Param("storeNm") String storeNm) throws URISyntaxException, IOException, ParserConfigurationException, SAXException {
+        Boolean response = patentService.checkDuplicated(storeNm);
+        return ResponseMapper.successOf(ApiStatus._OK, response, PatentController.class);
+    }
+}
