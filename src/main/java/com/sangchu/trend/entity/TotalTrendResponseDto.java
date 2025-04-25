7798af8ca1b58bdd2c819f3e3ffa34f3914f195f
KIMB0B <axzsd96301@gmail.com>
2025-04-25 14:37:58 +0900

    feat: API로 넘길 전체 트렌드 데이터 DTO 생성

---
+++ src/main/java/com/sangchu/trend/entity/TotalTrendResponseDto.java
@@ -0,0 +1,14 @@
+package com.sangchu.trend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class TotalTrendResponseDto {
+    private String keyword;
+    private Double recentCrtrYmRelevance;
+    private Map<String, Double> quarterCount;
+}
