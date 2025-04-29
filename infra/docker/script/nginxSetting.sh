6b52f3523b1e56355ff21388d7a0775a238b5e86
seykim <kksy0917@gmail.com>
2025-04-29 10:59:36 +0900

    Feat: 주석처리 된 코드들 수정
    Resolves: #33

--- infra/docker/script/nginxSetting.sh
+++ infra/docker/script/nginxSetting.sh
@@ -8,23 +8,23 @@ echo "Nginx started, waiting for SSL certificate..."
 # 인증서 디렉토리 확인
 echo "Checking if SSL certificates exist..."
 
-#if [ ! -f "/etc/letsencrypt/live/app.sangchu.xyz/fullchain.pem" ]; then
-#    echo "No certificate found, requesting certificate from certbot..."
-#
-#    # certbot을 실행하여 인증서를 발급받음
-#    certbot certonly --webroot --webroot-path=/var/www/certbot --email ${CERTBOT_EMAIL} --agree-tos --no-eff-email -d app.sangchu.xyz
-#
-#    # 인증서가 발급된 후 확인
-#    if [ -f "/etc/letsencrypt/live/app.sangchu.xyz/fullchain.pem" ]; then
-#        echo "Certificate issued successfully by certbot."
-#    else
-#        echo "Failed to issue certificate."
-#        sleep 1000000
-#        exit 1
-#    fi
-#else
-#    echo "Certificate found, skipping certbot."
-#fi
+if [ ! -f "/etc/letsencrypt/live/app.sangchu.xyz/fullchain.pem" ]; then
+    echo "No certificate found, requesting certificate from certbot..."
+
+    # certbot을 실행하여 인증서를 발급받음
+    certbot certonly --webroot --webroot-path=/var/www/certbot --email ${CERTBOT_EMAIL} --agree-tos --no-eff-email -d app.sangchu.xyz
+
+    # 인증서가 발급된 후 확인
+    if [ -f "/etc/letsencrypt/live/app.sangchu.xyz/fullchain.pem" ]; then
+        echo "Certificate issued successfully by certbot."
+    else
+        echo "Failed to issue certificate."
+        sleep 1000000
+        exit 1
+    fi
+else
+    echo "Certificate found, skipping certbot."
+fi
 
 # 인증서가 생성되면 Nginx 설정 파일을 config.conf로 교체
 echo "Updating Nginx configuration to use SSL..."
